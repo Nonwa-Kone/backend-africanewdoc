12d0148b14d9720d36644106df260aa68e7c7b29
Kone Nonwa <98397517+Nonwa-Kone@users.noreply.github.com>
2024-02-20 11:12:01 +0100

    Update ServiceMongoDB.js

--- src/services/ServiceMongoDB.js
+++ src/services/ServiceMongoDB.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 async function connectDB() {
-  await mongoose.connect(process.env.MONGODB_LOCAL);
+  await mongoose.connect(process.env.MONGODB_URL);
   console.log("Base de donnée connecté");
 }
 
